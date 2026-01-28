3cde78d782f74c762aa48bb4fef81425879a80a4
chacka1315 <chacka131@gmail.com>
2026-01-28 13:05:51 +0000

    Part before setting up the db.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Geist_Mono, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const geistMono = Geist_Mono({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['700', '400'],
+});
